d84f3848103a9a1fd94cef730b088446ef2ff1c2
wjsolano <wjsolano@espe.edu.ec>
2022-07-13 22:04:26 -0500

    Solicitar servicio

---
+++ src/app/componente-servicio/soli-servicio/soli-servicio.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-soli-servicio',
+  templateUrl: './soli-servicio.component.html',
+  styleUrls: ['./soli-servicio.component.scss'],
+})
+export class SoliServicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
